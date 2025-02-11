b799db63aa6b3a1b21c752a7b120771900bde215
iTranscend <llordbanks500@gmail.com>
2025-02-11 19:48:49 +0100

    feat: sample rust contract

---
+++ sample-contract/src/lib.rs
@@ -0,0 +1,38 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize)]
+struct InputData {
+    name: String,
+}
+
+#[derive(Serialize)]
+struct ContractResult {
+    message: String,
+}
+
+#[repr(C)]
+pub struct HandleResult {
+    ptr: u32,
+    len: u32,
+}
+
+#[no_mangle]
+pub extern "C" fn handle(input: *const u8, input_len: u32) -> *const HandleResult {
+    let input_data = unsafe { std::slice::from_raw_parts(input, input_len as usize) };
+    let input_json: InputData = serde_json::from_slice(input_data).unwrap();
+
+    let result = ContractResult {
+        message: format!("Hello {}", input_json.name),
+    };
+
+    let output = serde_json::to_vec(&result).unwrap().leak();
+    let pointer = output.as_mut_ptr();
+    let len = output.len() as u32;
+
+    let handle_result = Box::new(HandleResult {
+        ptr: pointer as _,
+        len,
+    });
+
+    Box::leak(handle_result) as _
+}
