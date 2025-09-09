824e5a4416149be4baae2af7dddacf2102d145d8
iTranscend <llordbanks500@gmail.com>
2025-09-09 23:59:44 +0100

    extend cli to support multiple contracts

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,6 @@ use clap::Parser;
 
 #[derive(Parser)]
 pub struct Args {
-    #[clap(short = 'p', long)]
-    pub contract_path: String,
+    #[clap(short = 'p', long = "contract-paths", value_delimiter = ',')]
+    pub contract_paths: Vec<String>,
 }
